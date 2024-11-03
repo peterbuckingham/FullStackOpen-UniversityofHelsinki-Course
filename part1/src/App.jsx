a9106858ec8fea9a468a42a4dc16941fd9ef2573
Peter Buckingham <peterbuckingham177@gmail.com>
2024-11-03 13:02:09 +0200

    Refractored code for 1.2

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -2,12 +2,21 @@ const Header = ({ course }) => {
   return <h1>{course}</h1>;
 };
 
-const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
+const Part = ({ name, exercises }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+// Content component
+const Content = ({ parts }) => {
   return (
     <div>
-      <p>{part1} {exercises1}</p>
-      <p>{part2} {exercises2}</p>
-      <p>{part3} {exercises3}</p>
+      <Part name={parts[0].name} exercises={parts[0].exercises} />
+      <Part name={parts[1].name} exercises={parts[1].exercises} />
+      <Part name={parts[2].name} exercises={parts[2].exercises} />
     </div>
   );
 };
@@ -16,22 +25,21 @@ const Total = ({ exercises }) => {
   return <p>Number of exercises {exercises}</p>;
 };
 
-
-
 const App = () => {
   const course = 'Half Stack application development';
-  const part1 = 'Fundamentals of React';
-  const exercises1 = 10;
-  const part2 = 'Using props to pass data';
-  const exercises2 = 7;
-  const part3 = 'State of a component';
-  const exercises3 = 14;
+
+  //parts array
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 },
+  ];
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} exercises1={exercises1} part2={part2} exercises2={exercises2} part3={part3} exercises3={exercises3} />
-      <Total exercises={exercises1 + exercises2 + exercises3} />
+      <Content parts={parts} />
+      <Total exercises={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
     </div>
   );
 };
